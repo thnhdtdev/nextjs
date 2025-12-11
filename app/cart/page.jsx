02046ddc928204a1087fc2c95ec169d8805fb256
thnhdtdev <thnhdat.dev@gmail.com>
2025-12-11 14:30:54 +0700

    ✨  feat: UI shopping cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSelector } from "react-redux";
+import { Trash2 } from "lucide-react";
 
 export default function CartList() {
 	const items = useSelector((state) => state.cart.items);
@@ -8,40 +9,41 @@ export default function CartList() {
 	if (!items.length) return <p>Giỏ hàng của bạn đang trống.</p>;
 
 	return (
-		<div className="max-w-5xl mx-auto py-10">
-			<h2 className="text-3xl font-bold mb-8 text-center text-gray-800">Giỏ hàng của bạn</h2>
-
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-red-50 p-6 rounded-xl shadow">
+		<div className="max-w-7xl bg-gray-50 flex flex-col gap-4 mx-auto p-8">
+			<div className="bg-white rounded-xl shadow-lg p-10">
+				<div className="font-bold text-4xl mb-6">Shopping cart</div>
+				<div className="grid grid-cols-6  mb-4 capitalize font-bold">
+					<div>Image</div>
+					<div className="col-span-2">title</div>
+					<div>quanlity</div>
+					<div>total price</div>
+					<div></div>
+				</div>
 				{items.map((item) => (
-					<div
-						key={item.id}
-						className="bg-white rounded-lg p-4 shadow flex flex-col items-center h-full"
-					>
+					<div className="grid grid-cols-6 items-center gap-4 rounded-xl border px-4 py-3 mb-4 bg-white shadow">
 						<img
 							src={item.image}
-							width={100}
-							height={100}
+							width={64}
+							height={64}
 							alt={item.title}
-							className="mb-3 object-contain rounded"
+							className="h-16 w-16 object-contain mx-auto"
 						/>
 
-						<span className="font-semibold text-base text-center mb-2">
-							{item.title}
-						</span>
+						<div className="flex flex-1 col-span-2 ">
+							<div className="font-semibold text-base line-clamp-1">{item.title}</div>
+						</div>
 
-						<span className="text-green-700 font-semibold mb-2">
-							Số lượng: <b>{item.quantity}</b>
-						</span>
+						<div className="flex items-center gap-2">
+							<span className="w-6 text-center">{item.quantity}</span>
+						</div>
 
-						<div className="flex-1" />
-
-						<span className="text-gray-600 mt-auto">
-							Giá: {item.price} x {item.quantity} ={" "}
-							<b>{item.price * item.quantity}</b>
-						</span>
-
-						<button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded transition w-full">
-							Delete
+						<div>{item.quantity * item.price}</div>
+						<button
+							className="text-gray-400 hover:text-red-500 ml-3"
+							onClick={() => onRemove(item.id)}
+							title="Remove"
+						>
+							<Trash2 size={20} />
 						</button>
 					</div>
 				))}
