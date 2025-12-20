d24527041313b9b35d19b2fde81ce4a277d038bb
thnhdtdev <thnhdat.dev@gmail.com>
2025-12-20 16:32:33 +0700

    ✨  feat: config fullUrl logging and add login page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	logging: {
+		fetches: {
+			fullUrl: true
+		}
+	}
+	/* config options here */
 };
 
 export default nextConfig;
